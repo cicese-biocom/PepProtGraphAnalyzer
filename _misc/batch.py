9571fd996a96e0dbfac4a950dded1757aebd9bcf
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2025-01-02 10:16:43 -0800

    refactor: Remove files with refactored code in other files

--- _misc/batch.py
+++
@@ -1,7 +0,0 @@
-from itertools import islice
-
-
-def batch(iterable, batch_size):
-    iterator = iter(iterable)
-    for first in iterator:
-        yield [first] + list(islice(iterator, batch_size - 1))
