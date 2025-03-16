333fdef901d2efb349e534b57dc606025b770e73
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2025-03-16 00:45:11 -0700

    feat(arguments_parser): Add new argument validations and environment variable loading

--- module/argument_parser.py
+++ module/argument_parser.py
@@ -1,16 +1,31 @@
-from datetime import datetime
 from enum import Enum, auto
-from pathlib import Path
+
+from dotenv import load_dotenv
+from pydantic import PositiveInt, PositiveFloat
 from pydantic.v1 import BaseModel, Field, root_validator, FilePath, DirectoryPath
 import pydantic_argparse
-from typing import Optional, Literal
-from utils import json_parser
+from typing import Optional, Literal, List, Annotated
+from module.distance import EuclideanDistance, CanberraDistance, LanceWilliamsDistance, ClarkDistance, SoergelDistance, \
+    BhattacharyyaDistance, AngularSeparationDistance
+from util.json_parser import get_distance_intervals, output_config
 
 
 class TertiaryStructurePredictionMethod(Enum):
     esmfold = auto()
     alphafold = auto()
 
+
+distance_function_map = {
+    'euclidean': EuclideanDistance(),
+    'canberra': CanberraDistance(),
+    'lance_williams': LanceWilliamsDistance(),
+    'clark': ClarkDistance(),
+    'soergel': SoergelDistance(),
+    'bhattacharyya': BhattacharyyaDistance(),
+    'angular_separation': AngularSeparationDistance()
+}
+DISTANCE_FUNCTIONS = list(distance_function_map.keys())
+DISTANCE_STRATEGIES = list(distance_function_map.values())
 
 PREDICTION_METHODS = {TertiaryStructurePredictionMethod.esmfold}
 LOAD_METHODS = {TertiaryStructurePredictionMethod.esmfold, TertiaryStructurePredictionMethod.alphafold}
@@ -21,50 +36,19 @@ class CommonArguments(BaseModel):
         description="Path to the input dataset in csv format."
     )
 
-    batch_size: Optional[int] = Field(
-        default=512,
-        description="Batch size"
-    )
-
     output_path: DirectoryPath = Field(
         description="The path to save the outputs"
     )
 
     @root_validator
     def validator(cls, values):
-        # output paths
-        values['output_paths'] = cls._output_paths(**values)
+        # distance functions
+        values['distance_functions'] = DISTANCE_FUNCTIONS
 
-        # distance function
-        values['distance_functions'] = ['euclidean', 'canberra', 'lance_williams', 'clark', 'soergel', 'bhattacharyya',
-                                        'angular_separation']
+        # output paths
+        values['output_paths'] = output_config(**values)
 
         return values
-
-    def _output_paths(**kwargs):
-        output_path = kwargs.get('output_path')
-        mode = kwargs.get('mode')
-        mode_path_name = kwargs.get('mode_path_name')
-
-        # create output path
-        current_time = datetime.now().replace(microsecond=0).isoformat().replace(':', '.')
-        output_path = output_path.joinpath(f"{current_time}-{mode_path_name}")
-        output_path.mkdir(parents=True)
-
-        # load output path settings
-        settings_file = Path('settings/output_settings.json').resolve()
-        data = json_parser.load_json(settings_file)
-
-        # create output file paths
-        paths = {}
-        for setting in data["output_settings"]:
-            if mode in setting["modes"]:
-                paths[setting["key"]] = output_path.joinpath(setting["name"])
-                paths[setting["key"]].mkdir(parents=True, exist_ok=True)
-
-                for file in setting.get("files", []):
-                    paths[file["key"]] = paths[setting["key"]].joinpath(file["name"])
-        return paths
 
 
 class DataIngestionArguments(CommonArguments):
@@ -100,6 +84,11 @@ class DataIngestionArguments(CommonArguments):
         description="Amino acid representations"
     )
 
+    batch_size: Optional[int] = Field(
+        default=512,
+        description="Batch size"
+    )
+
     @root_validator
     def validator(cls, values):
         super(DataIngestionArguments, cls).validator(values)
@@ -119,6 +108,9 @@ class DataIngestionArguments(CommonArguments):
                     f"Valid methods are: {', '.join(m.name for m in LOAD_METHODS)}."
                 )
 
+        # distance strategy
+        values['distance_strategies'] = DISTANCE_STRATEGIES
+
         return values
 
 
@@ -130,15 +122,51 @@ class GraphAnalyzerArguments(CommonArguments):
         description="Path to json file with distance intervals"
     )
 
+    batch_size: Annotated[Optional[PositiveInt], Field(description='Batch size')] = 1000
+
+    graph_similarity_functions: List[Literal['cosine_similarity', 'graph_edit_distance', 'optimize_graph_edit_distance']] = Field(description='Functions to calculate similarities')
+
+    ged_timeout: Annotated[Optional[PositiveFloat],
+                           Field(description="Maximum number of seconds to execute. After timeout is met, the current best GED is returned.")]
+
+    batch_size: Optional[int] = Field(
+        default=512,
+        description="Batch size"
+    )
+
     @root_validator
     def validator(cls, values):
-        super(GraphAnalyzerArguments, cls).validator(values)        
-        
+        super(GraphAnalyzerArguments, cls).validator(values)
+
+        # graph similarity functions
+        if 'graph_edit_distance' in values['graph_similarity_functions'] and 'optimize_graph_edit_distance' in values['graph_similarity_functions']:
+            raise ValueError(
+                "Only one of 'graph_edit_distance' or 'optimize_graph_edit_distance' can be used at the same time.")
+
+        # ged_timeout parameter
+        if 'graph_edit_distance' not in values['graph_similarity_functions'] and values['ged_timeout']:
+            raise ValueError(
+                f"The graph similarity functions '{','.join(values['graph_similarity_functions'])}' "
+                f"do not require the parameter 'ged_timeout'."
+            )
+
+        # distance intervals json
+        values['distance_intervals'] = get_distance_intervals(values['distance_intervals_json'])
+        del values['distance_intervals_json']
+
+        values['random_graph_sim_funcs'] = ['cosine_similarity']
+
         return values
+
 
 class SequenceAnalyzerArguments(CommonArguments):
     mode: str = Field("sequence_analyzer", const=True)
     mode_path_name: str = Field("Sequence_Analyzer", const=True)
+
+    batch_size: Optional[int] = Field(
+        default=512,
+        description="Batch size"
+    )
 
     @root_validator
     def validator(cls, values):
@@ -153,6 +181,7 @@ def argument_parser(mode: str):
         'graph_analyzer': GraphAnalyzerArguments
     }
 
+    load_dotenv(dotenv_path='.env')
     parser = pydantic_argparse.ArgumentParser(model=model[mode], exit_on_error=False)
     arguments = parser.parse_typed_args()
     return arguments
