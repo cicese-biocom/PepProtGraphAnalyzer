f32932ec6886edac4a535714fbeb6d3b59f2e5c3
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2025-03-16 01:13:16 -0700

    refactor(sh): Update graph_analyzer.sh script

--- graph_analyzer.sh
+++ graph_analyzer.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-dataset="datasets/StarPep/StarPep.csv"
-pdb_path="datasets/StarPep/ESMFold_pdbs/"
-tertiary_structure_method='esmfold'
-amino_acid_representation="CA"
-batch_size=56
-distance_intervals_json_path='datasets/json/distance_intervals.json'
-output_path='output/StarPep/Paper/'
-
 python graph_analyzer.py \
-    --dataset "$dataset" \
-    --pdb_path "$pdb_path" \
-    --tertiary_structure_method="$tertiary_structure_method"  \
-    --amino_acid_representation="$amino_acid_representation" \
-    --batch_size="$batch_size" \
-    --distance_intervals_json_path="$distance_intervals_json_path" \
-    --output_path="$output_path"
+  --dataset datasets/AVPDiscover/AVPDiscover.csv \
+  --output-path output/Test/ \
+  --distance-intervals-json datasets/json/starpep_intervals.json \
+  --graph-similarity-functions cosine_similarity graph_edit_distance \
+  --ged-timeout 10 \
+  --batch-size 1000
