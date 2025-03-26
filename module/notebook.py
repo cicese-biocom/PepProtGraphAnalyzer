ca9e0a59728bf9b9d279bb7d54c2d67ac31b06c8
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2025-03-26 14:03:23 -0700

    refactor(notebook): Refactored the logic of the notebook.py module

--- module/notebook.py
+++ module/notebook.py
@@ -1,9 +1,13 @@
+import logging
+import os
+from pathlib import Path
+
 import pandas as pd
 from dotenv import load_dotenv
 from pydantic import BaseModel, FilePath, Field, root_validator, DirectoryPath
 from module.application_context import ApplicationContext
 from module.logger_handler import LoggerHandler
-from util.json_parser import output_config
+from util.json_parser import output_config, input_config
 
 DISTANCE_FUNCTIONS = ['euclidean', 'canberra', 'lance_williams', 'clark', 'soergel', 'bhattacharyya', 'angular_separation']
 
@@ -16,7 +20,7 @@ class NotebookArguments(BaseModel):
     def validator(cls, values):
         # mode
         values['mode'] = "notebook"
-        values['mode_path_name'] = "Notebook"
+        values['mode_path_name'] = "Notebook_Analysis"
 
         # distance functions
         values['distance_functions'] = DISTANCE_FUNCTIONS
@@ -27,11 +31,48 @@ class NotebookArguments(BaseModel):
         return values
 
 
-class DBClient:
-    def __init__(self, dataset_path, output_path):
-        load_dotenv(dotenv_path=".env.jupyter")
-        self._args = NotebookArguments(dataset=dataset_path, output_path=output_path)
-        self._context = ApplicationContext(**self._args.dict())
+class NotebookApp:
+    _instance = None  # This is the single instance of the class
+
+    def __new__(cls, dotenv_path):
+        if cls._instance is None:
+            # Create a new instance only if one doesn't already exist
+            cls._instance = super(NotebookApp, cls).__new__(cls)
+            cls._instance._init(dotenv_path)
+
+        config = {
+            'dataset_path': Path(os.getenv("DATASET_PATH")).resolve(),
+            'pdb_path': Path(os.getenv("PDB_PATH")).resolve(),
+            'notebook_output_path': Path(os.getenv("NOTEBOOK_OUTPUT_PATH")).resolve(),
+            'sequence_analysis_path': Path(os.getenv("SEQUENCE_ANALYSIS_PATH")).resolve(),
+            'graph_analysis_path': Path(os.getenv("GRAPH_ANALYSIS_PATH")).resolve(),
+        }
+
+        logging.getLogger('logger').info(
+            "Setting:\n" + "\n".join(f"{key}: {value}" for key, value in config.items())
+        )
+
+        return cls._instance
+
+    def _init(self, dotenv_path):
+        load_dotenv(dotenv_path=dotenv_path)
+
+        dataset_path = Path(os.getenv("DATASET_PATH")).resolve()
+        output_path = Path(os.getenv("NOTEBOOK_OUTPUT_PATH")).resolve()
+
+        self._pdb_path = Path(os.getenv("PDB_PATH")).resolve()
+        self._config = NotebookArguments(dataset=dataset_path, output_path=output_path)
+        self._context = ApplicationContext(**self._config.dict())
+
+        self._sequence_analysis = input_config(
+                input_path=Path(os.getenv("SEQUENCE_ANALYSIS_PATH")).resolve(),
+                mode=self._config.mode
+        )
+
+        self._graph_analysis = input_config(
+                input_path=Path(os.getenv("GRAPH_ANALYSIS_PATH")).resolve(),
+                mode=self._config.mode
+        )
 
         # Step 1: Initialize logger
         self._init_logger()
@@ -48,28 +89,52 @@ class DBClient:
     def data(self):
         return self._data
 
+    @property
+    def pdb_path(self):
+        return self._pdb_path
+
+    @property
+    def sequence_analysis(self):
+        return self._sequence_analysis
+
+    @property
+    def graph_analysis(self):
+        return self._graph_analysis
+
+    @property
+    def graph_analysis(self):
+        return self._graph_analysis
+
+    def paths(self):
+        return self._config.output_paths
+
     def _init_logger(self) -> None:
         LoggerHandler.init_logger(
-            log_output_path=self._args.output_paths['log_file']
+            log_output_path=self._config.output_paths['log_file']
         )
 
     def _load_data(self) -> pd.DataFrame:
         return self._context.data_loader.read_file(
-            filepath=self._args.dataset
+            filepath=self._config.dataset
         )
 
     def _validate_data(self, data: pd.DataFrame) -> pd.DataFrame:
         data = self._context.data_validator.validate(
             data=data,
-            output_paths=self._args.output_paths
+            output_paths=self._config.output_paths
         )
         return data
 
     def _prepare_data(self, data):
         return self._context.db_service.prepare_data(data)
 
-    def get_sequences_with_max_distance(self, distance_function):
-        sequences_with_max_distance = self._context.db_service.get_sequences_with_max_distance(distance_function)
-        columns = ['sequence', 'aa_src', 'aa_dst', 'aa_src', *self._args.distance_functions]
+    def get_sequences_with_largest_distance(self, distance_function):
+        sequences_with_largest_distance = self._context.db_service.get_sequences_with_largest_distance(distance_function)
+        columns = ['sequence', 'aa_src', 'aa_dst', *self._config.distance_functions]
+        return sequences_with_largest_distance[columns]
 
-        return sequences_with_max_distance[columns]
+    def get_distance_values(self, distance_functions):
+        return self._context.db_service.get_distance_values(distance_functions)
+
+    def get_distance_values_and_pivot(self, distance_functions, sequence=None):
+        return self._context.db_service.get_distance_values_and_pivot(distance_functions, sequence)
