de32eb1a5e1e2813dc03288f01cf321a0180a48c
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2025-03-26 23:55:11 -0700

    refactor(sh): Update parameter name

--- data_ingestion.sh
+++ data_ingestion.sh
@@ -4,13 +4,13 @@ dataset="datasets/StarPep/StarPep.csv"
 pdb_path="datasets/StarPep/ESMFold_pdbs/"
 tertiary_structure_method='esmfold'
 output_path='output/StarPep/PaperNew/'
-minimum_sequence_length=10
-maximum_sequence_length=100
+min_sequence_len=10
+max_sequence_len=100
 
 python data_ingestion.py \
     --dataset "$dataset" \
     --pdb-path "$pdb_path" \
     --tertiary-structure-method="$tertiary_structure_method" \
     --output-path="$output_path" \
-    --minimum-sequence-length="$minimum_sequence_length" \
-    --maximum-sequence-length="$maximum_sequence_length"
+    --min-sequence-len="$min_sequence_len" \
+    --max-sequence-len="$max_sequence_len"
