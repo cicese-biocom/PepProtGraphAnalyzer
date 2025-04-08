dd16cfdafa0766ce22c465decd72ebe0c64a61a3
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2025-04-08 10:32:48 -0700

    refactor(sh): Update parameters

--- sequence_analyzer.sh
+++ sequence_analyzer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 python sequence_analyzer.py \
-  --dataset dataset/AVPDiscover/AVPDiscover-test.csv \
-  --output-path output/Paper_AVP/AVPDiscover/Test/ \
+  --dataset dataset/AVPDiscover/AVP-external.csv \
+  --output-path output/results/AVPDiscover/Ext \
+  --min-seq-len 10 \
+  --max-seq-len 100 \
   --batch-size 1000
